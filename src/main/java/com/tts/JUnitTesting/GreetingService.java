f2b4c25731d58651d12b75bd34468987a8d9adf4
Jordan <71449357+sbjordan222@users.noreply.github.com>
2021-03-09 22:39:58 -0600

    first commit

---
+++ src/main/java/com/tts/JUnitTesting/GreetingService.java
@@ -0,0 +1,10 @@
+package com.tts.JUnitTesting;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+	public String greet() {
+		return "Hello, World";
+	}
+}
